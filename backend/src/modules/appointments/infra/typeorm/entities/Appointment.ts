b2580e0e612d41c3598f6af4366cb0813725c6ed
WagnerFilipeMoura <37957196+WagnerFilipeMoura@users.noreply.github.com>
2020-11-12 23:26:33 -0300

    TypeOrm
    Database configuration, using TypeOrm

---
+++ backend/src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  @Column()
+  recipient: string;
+
+  @Column()
+  message: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Appointment;
---
+++ backend/src/shared/infra/typeorm/migrations/1605231271846-CreateTableAppointments.ts
@@ -0,0 +1,51 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateTableAppointments1605231271846 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name: 'appointments',
+          columns: [
+            {
+              name: 'id',
+              type: 'uuid',
+              isPrimary: true,
+              generationStrategy: 'uuid',
+              default: 'uuid_generate_v4()',
+            },
+            {
+              name: 'date',
+              type: 'timestamp with time zone',
+              isNullable: false,
+            },
+            {
+              name: 'recipient',
+              type: 'varchar',
+              isNullable: false
+            },
+            {
+              name: 'message',
+              type: 'varchar',
+              isNullable: false
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+          ]
+        })
+      )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('appointments');
+    }
+
+}
