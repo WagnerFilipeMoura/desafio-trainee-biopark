71e94f7cbaaf58293df6039e26cac7e63b69585d
WagnerFilipeMoura <37957196+WagnerFilipeMoura@users.noreply.github.com>
2020-11-14 11:24:34 -0300

    Create Appointment
    Appointment save in database, add dependency injection in class.
    
    Problem librery date-fns, verify the module.

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,16 +1,36 @@
+import { isBefore, startOfHour, isDate, parseISO, format } from "date-fns";
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
 interface IRequest {
   date: Date;
   recipient: string;
   message: string;
 }
 
+@injectable()
 class CreateAppointmentService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
   public async execute({
     date,
     recipient,
     message
-  }: IRequest) {
-    return {date, recipient, message}
+  }: IRequest): Promise<Appointment> {
+    const appointment = await this.appointmentsRepository.create({
+      date,
+      recipient,
+      message,
+    });
+
+    return appointment;
   }
 }
 
