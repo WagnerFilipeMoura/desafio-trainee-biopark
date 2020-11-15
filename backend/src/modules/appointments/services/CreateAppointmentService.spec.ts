82355f01be78c34d23c776d68cb645dc587951ef
WagnerFilipeMoura <37957196+WagnerFilipeMoura@users.noreply.github.com>
2020-11-15 11:24:32 -0300

    Test Create Appointment

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,7 +1,17 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
 
+    const appointment = await createAppointment.execute({
+      date: String(new Date()),
+      recipient: 'Wagner',
+      message: 'Teste',
+    })
+
+    expect(appointment).toHaveProperty('id');
   })
 })
